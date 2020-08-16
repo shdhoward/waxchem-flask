95049b413f5e2419236b5f4c616165dbd9f3b7fa
David Howard <shdhoward@gmail.com>
2020-08-16 12:17:16 +0800

    added dbconnect

--- waxchem_flask/waxchem_flask/waxchem_flask/views.py
+++ waxchem_flask/waxchem_flask/waxchem_flask/views.py
@@ -5,6 +5,7 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template
 from waxchem_flask import app
+import pypyodbc  
 
 @app.route('/')
 @app.route('/home')
@@ -16,19 +17,9 @@ def home():
         year=datetime.now().year,
     )
 
-@app.route('/mdhdb')
-def contact():
-    """Renders the contact page."""
-    return render_template(
-        'mdhdb.html',
-        title='MDHDB',
-        year=datetime.now().year,
-        message='MDHDB page.'
-    )
-
 @app.route('/surface')
 def about():
-    """Renders the about page."""
+    """Renders the surface page."""
     return render_template(
         'surface.html',
         title='Surface samples',
@@ -36,12 +27,38 @@ def about():
         message='Surface samples page.'
     )
 
-@app.route('/collars')
+@app.route('/downhole')
 def collars():
+    """Renders the downhole page."""
+
+    ## creating connection Object which will contain SQL Server Connection    
+    connection = pypyodbc.connect('Driver={SQL Server};Server=nitro5;Database=drillholes;Trusted_Connection=yes; ')# Creating Cursor    
+    
+    cursor = connection.cursor()    
+    cursor.execute("SELECT top 15 * FROM Collar")    
+    data = cursor.fetchall()
+    #s = "<table style='border:1px solid red'>"    
+    #for row in cursor:    
+    #    s = s + "<tr>"    
+    #    for x in row:    
+    #        s = s + "<td>" + str(x) + "</td>"    
+    #s = s + "</tr>"    
+    connection.close() 
 
     return render_template(
-        'collars.html',
-        title='Collars',
+        'downhole.html',
+        title='Downhole samples',
         year=datetime.now().year,
-        message='Collars page.'
+        message='Downhole samples page.',
+        data = data
+    )
+
+@app.route('/mdhdb')
+def contact():
+    """Renders the mdhdb page."""
+    return render_template(
+        'mdhdb.html',
+        title='MDHDB',
+        year=datetime.now().year,
+        message='MDHDB page.'
     )
